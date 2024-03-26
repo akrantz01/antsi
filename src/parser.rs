802f796fbe905f678700e6c7e0d472b4a07cad10
Alex Krantz <alex@krantz.dev>
2024-03-26 11:02:25 -0700

    Port tokenization tests from nom implementation

--- src/parser.rs
+++ src/parser.rs
@@ -185,6 +185,188 @@ mod tests {
     }
 
     #[test]
+    fn lowercase_alphabetic() {
+        with_source!("abcdef", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("abcdef"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn uppercase_alphabetic() {
+        with_source!("ABCDEF", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("ABCDEF"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn mixed_case_alphabetic() {
+        with_source!("aBcDeF", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("aBcDeF"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn numeric() {
+        with_source!("123456", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("123456"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn lowercase_alphanumeric() {
+        with_source!("abc123", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("abc123"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn uppercase_alphanumeric() {
+        with_source!("ABC123", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("ABC123"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn mixed_case_alphanumeric() {
+        with_source!("AbCd1234", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("AbCd1234"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn special_characters() {
+        with_source!("!@#$%^", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("!@#$%^"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn mixed_characters() {
+        with_source!("ABCdef123!@#", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("ABCdef123!@#"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn escaped_characters() {
+        with_source!("\\(\\)\\[\\]", |result, errors| {
+            assert_eq!(result, vec![Token::Content(String::from("()[]"))]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn escaped_whitespace() {
+        with_source!("\\ \n\t", |result, errors| {
+            assert_eq!(result, vec![]);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn empty_token() {
+        with_source!("[fg:red]()", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_foreground() {
+        with_source!("[fg:red](inner)", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_background() {
+        with_source!("[bg:blue](inner)", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_single_decoration() {
+        with_source!("[deco:dim](inner)", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_multiple_decorations() {
+        with_source!("[deco:dim,italic](inner)", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_multiple_styles() {
+        with_source!(
+            "[deco:dim,italic;fg:red;bg:blue](inner)",
+            |result, errors| {
+                assert_snapshot!(result);
+                assert!(errors.is_empty());
+            }
+        );
+    }
+
+    #[test]
+    fn token_with_leading_content() {
+        with_source!("leading [fg:red](content)", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_trailing_content() {
+        with_source!("[fg:red](content) trailing", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn token_with_leading_and_trailing_content() {
+        with_source!("leading [fg:red](content) trailing", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn nested_token() {
+        with_source!("[fg:red]([bg:blue](inner))", |result, errors| {
+            assert_snapshot!(result);
+            assert!(errors.is_empty());
+        });
+    }
+
+    #[test]
+    fn kitchen_sink() {
+        with_source!(
+            "leading [fg:red](one [bg:blue](two [deco:dim](three) two) one) trailing",
+            |result, errors| {
+                assert_snapshot!(result);
+                assert!(errors.is_empty());
+            }
+        );
+    }
+
+    #[test]
     fn parse_unescaped_open_parenthesis_in_plaintext() {
         with_source!("before ( after", |result, errors| {
             assert_snapshot!({ snapshot_suffix => "result" }, result);
